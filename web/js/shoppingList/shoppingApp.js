f27ad5a108481f73430d8d09e263c0b29853024c
Barna001 <burombarna@gmail.com>
2016-05-13 19:48:59 +0200

    Simple structure for angular

---
+++ web/js/shoppingList/shoppingApp.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Barna on 2016.05.13..
+ */
+var app = angular.module("shoppingApp", []);
---
+++ web/js/shoppingList/shoppingController.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Barna on 2016.05.13..
+ */
+app.controller("shoppingController", function($scope) {
+    $scope.products = ["Milk", "Bread", "Cheese"];
+    $scope.addItem = function () {
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}
+        if ($scope.products.indexOf($scope.addMe) == -1) {
+            $scope.products.push($scope.addMe);
+        } else {
+            $scope.errortext = "The item is already in your shopping list.";
+        }
+    }
+    $scope.removeItem = function (x) {
+        $scope.errortext = "";
+        $scope.products.splice(x, 1);
+    }
+});
