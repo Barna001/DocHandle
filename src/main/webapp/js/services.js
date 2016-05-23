afc5ba6d062a3ee1683901930c0b585224879163
Barna001 <burombarna@gmail.com>
2016-05-23 19:29:29 +0200

    Some stuff with ngdemo not find

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -13,3 +13,87 @@ services.factory('UserFactory', function ($resource) {
         }
     })
 });
+
+services.factory('UserService',UserService)
+
+UserService.$inject = [ '$http', '$q' ];
+function UserService($http, $q) {
+
+    var service = {
+        saveUser : saveUser,
+        getUser : getUser
+        //getUsers : getUsers,
+        //updateUser : updateUser,
+        //deleteUser: deleteUser,
+        //getCurrentUser : getCurrentUser
+    };
+
+    return service;
+
+    function saveUser(userName) {
+        var deferred = $q.defer();
+        $http.post("rest/users/new", userName).success(function(data, status) {
+            deferred.resolve(data);
+        }).error(function(status) {
+            deferred.reject(status);
+        });
+        return deferred.promise;
+    }
+
+    function getUser(name) {
+        var deferred = $q.defer();
+        $http.get("rest/users/userByName",{params:{name:name}}).success(function(data, status) {
+            deferred.resolve(data);
+        }).error(function(data, status) {
+            deferred.reject(status);
+        });
+        return deferred.promise;
+    }
+
+    //function getUsers(){
+    //    var deferred = $q.defer();
+    //    $http.get("api/user").success(function(data, status) {
+    //        deferred.resolve(data);
+    //    }).error(function(data, status) {
+    //        deferred.reject(status);
+    //    });
+    //    return deferred.promise;
+    //}
+    //
+    //function updateUser(user) {
+    //    user.id = user.userId;
+    //    //must delete, because the PUT would not work
+    //    delete user['links'];
+    //    delete user['userId'];
+    //    delete user['password-re'];
+    //    var deferred = $q.defer();
+    //    $http.put("api/user/"+user.id, user).success(function(data, status) {
+    //        deferred.resolve(data);
+    //    }).error(function(data, status) {
+    //        deferred.reject(status);
+    //    });
+    //    return deferred.promise;
+    //
+    //}
+    //
+    //function getCurrentUser() {
+    //    var deferred = $q.defer();
+    //    $http.get("api/user/username").success(function(data, status) {
+    //        deferred.resolve(data);
+    //    }).error(function(status) {
+    //        deferred.reject(status);
+    //    });
+    //    return deferred.promise;
+    //}
+    //
+    //function deleteUser(id) {
+    //    var deferred = $q.defer();
+    //    $http.delete("api/user/"+id).success(function(data, status) {
+    //        deferred.resolve(data);
+    //    }).error(function(status) {
+    //        deferred.reject(status);
+    //    });
+    //    return deferred.promise;
+    //}
+
+}
