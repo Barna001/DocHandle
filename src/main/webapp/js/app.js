cc5a14d57f441b81d6ffbef7ba5b4991809e72b4
Barna001 <burombarna@gmail.com>
2016-12-12 22:07:58 +0100

    Add new pages for creation(except file)

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,11 +2,15 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('docHandler', ['docHandler.filters', 'docHandler.services', 'docHandler.directives', 'docHandler.controllers']).config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'})
+    $routeProvider
         .when('/users', {templateUrl: 'partials/users.html', controller: 'UserController'})
+        .when('/createUser', {templateUrl: 'partials/createUser.html', controller: 'UserController'})
         .when('/documents', {templateUrl: 'partials/documents.html', controller: 'DocumentController'})
+        .when('/createDocument', {templateUrl: 'partials/createDocument.html', controller: 'DocumentController'})
         .when('/files', {templateUrl: 'partials/files.html', controller: 'FileController'})
         .when('/userGroups', {templateUrl: 'partials/userGroups.html', controller: 'UserGroupController'})
-        .when('/documentGroups', {templateUrl: 'partials/documentGroups.html', controller: 'DocumentGroupController'});
+        .when('/createUserGroup', {templateUrl: 'partials/createUserGroup.html', controller: 'UserGroupController'})
+        .when('/documentGroups', {templateUrl: 'partials/documentGroups.html', controller: 'DocumentGroupController'})
+        .when('/createDocumentGroup', {templateUrl: 'partials/createDocumentGroup.html', controller: 'DocumentGroupController'});
     $routeProvider.otherwise({redirectTo: '/users'});
 }]);
